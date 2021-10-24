e1201d40783661a1c08d374398bdbff1e9db913c
kabu <kabukunz@gmail.com>
2021-10-24 19:20:30 +0200

    default install dir

--- cmake/libigl.cmake
+++ cmake/libigl.cmake
@@ -304,7 +304,6 @@ if(LIBIGL_WITH_MMG)
         https://github.com/MmgTools/mmg.git
         379209a9bb9b52df5e7a6ca08ae366bf1991960f
         SOURCE_DIR "${LIBIGL_EXTERNAL}"
-        INSTALL_DIR "${LIBIGL_EXTERNAL}/prebuilt"
         PACKAGE_OPTIONS ${PACKAGE_OPTIONS}
         )    
 
